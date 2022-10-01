b28e92203d3170f49663865a3178e212145cb40a
Salomon Nghukam <salomon.noussi@nanobnk.com>
2022-10-01 12:56:29 +0100

    set default page

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import {
     Login,
     ManagerHome,
@@ -29,22 +29,33 @@ root.render(
                 <Routes>
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
-                    <Route path="/" element={<App />}>
-                        <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
-                            <Route path="/home" element={<UserHome />} />
-                            <Route path="/resa" element={<Reservation />} />
-                        </Route>
-                        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
-                            <Route path="/admin" element={<ManagerHome />}>
-                                <Route path="bike/:bikeId" element={<BikeReservations />} />
+                    <Route element={<RequireAuth allowedRoles={[ROLES.User, ROLES.Admin]} />}>
+                        <Route path="/" element={<App />}>
+                            <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+                                <Route path="/home" element={<UserHome />} />
+                                <Route path="/resa" element={<Reservation />} />
                             </Route>
-                            <Route path="/users" element={<Users />}>
-                                <Route path=":userId" element={<UserReservations />} />
+                            <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+                                <Route path="/admin" element={<ManagerHome />}>
+                                    <Route path="bike/:bikeId" element={<BikeReservations />} />
+                                </Route>
+                                <Route path="/users" element={<Users />}>
+                                    <Route path=":userId" element={<UserReservations />} />
+                                </Route>
+                                <Route path="/managers" element={<Managers />} />
+                                <Route path="/reservations" element={<Reservation />} />
                             </Route>
-                            <Route path="/managers" element={<Managers />} />
-                            <Route path="/reservations" element={<Reservation />} />
                         </Route>
                     </Route>
+                    <Route
+                        path="*"
+                        element={
+                            <main style={{ padding: "1rem", marginTop: "100px" }}>
+                                <p>There's nothing here!</p>
+                                <Link to={"/login"}> Home </Link>
+                            </main>
+                        }
+                    />
                 </Routes>
             </AuthProvider>
         </BrowserRouter>
