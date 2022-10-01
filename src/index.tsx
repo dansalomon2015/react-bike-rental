502d3ac33da3dca89d78f256646d8cff7f490373
Salomon Nghukam <salomon.noussi@nanobnk.com>
2022-10-01 10:24:01 +0100

    ci/cd pipeline setup

--- src/index.tsx
+++ src/index.tsx
@@ -18,6 +18,9 @@ import { RequireAuth } from "components/";
 import { ROLES } from "utils/";
 import { AuthProvider } from "context";
 
+import LogRocket from "logrocket";
+LogRocket.init("z6i2k5/biker-rentals");
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
